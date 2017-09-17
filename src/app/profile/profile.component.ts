a8406862f773ef372d97ba4c1d6b0d10e18020b1
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2017-09-17 13:35:38 +1200

    target mobile portrait first
    - fit home page on iPhone5
    - new `work/portrait` page
    - scrolling `profile` background image
    - show text/thumbnail on image click
    - avoid animation on ngIf

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { pageAnimation } from '../app.animation';
 
 @Component({
@@ -8,10 +8,27 @@ import { pageAnimation } from '../app.animation';
   //host: {'[@pageAnimation]': ''}
 })
 export class ProfileComponent implements OnInit {
+  state: string = "paused";
+  isTouchDevice: boolean;
+  @ViewChild('background') elementRef: ElementRef;
+  
+  set animationPlayState(state) { this.elementRef.nativeElement.style['animation-play-state'] = state; }
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.isTouchDevice = window.matchMedia("(pointer:coarse)").matches;
+
+    console.warn(`'ProfileComponent' ${this.isTouchDevice}`);
+    //this.animationPlayState = this.state;
+  }
+
+  toggleState() {
+    this.state = this.state === 'paused' ? 'running' : 'paused';
+    
+    if (this.isTouchDevice) {
+      this.animationPlayState = this.state;
+    }
   }
 
 }
