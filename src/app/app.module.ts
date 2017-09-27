aa8b86ceef60404ef2a2c499f1b689f4a45cab58
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2017-09-27 13:40:28 +1300

    support IE11
    - main min-height calc not working: specify height
    - polyfills
    - main background-color ignored: specify it again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,22 +22,22 @@ import { ContactComponent } from './contact/contact/contact.component';
 //import { GalleryParentComponent } from './gallery/gallery-parent.component';
 //import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
 
-import 'web-animations-js/web-animations.min';
+//import 'web-animations-js/web-animations.min';
 import 'hammerjs/hammer';
 
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/set';
 
 const ROUTES: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
