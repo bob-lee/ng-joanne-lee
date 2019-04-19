9236baa6fdbb83571c2463bf904d7fccebcb4f9c
Bob Lee <bob.bumsuk.lee@gmail.com>
2019-04-19 15:25:21 +1200

    update angular from 4 to 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 // import { ServiceWorkerModule } from '@angular/service-worker';
 
@@ -45,7 +45,7 @@ export const ROUTES: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     // ServiceWorkerModule,
     RouterModule.forRoot(ROUTES)
   ],
