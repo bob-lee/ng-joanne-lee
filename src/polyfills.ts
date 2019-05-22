768ac5472f882461ea6784558389d5ab575fdc1c
Bob Lee <bob.bumsuk.lee@gmail.com>
2019-05-22 12:18:29 +1200

    remove 'web animation' polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,7 +45,7 @@ import 'core-js/es6/reflect';
  * Required to support Web Animations `@angular/animation`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
